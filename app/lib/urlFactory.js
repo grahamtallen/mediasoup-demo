e26c76871565f77b45c9ab779f5f340e50746825
Graham Allen <grahamtallen94@gmail.com>
2019-05-28 23:40:57 -0500

    messing with the websocket uril

--- app/lib/urlFactory.js
+++ app/lib/urlFactory.js
@@ -1,7 +1,7 @@
 export function getProtooUrl(peerName, roomId, forceH264)
 {
 	const hostname = window.location.hostname;
-	let url = `ws://localhost:3443/?peerName=${peerName}&roomId=${roomId}`;
+	let url = `wss://${hostname}:80/?peerName=${peerName}&roomId=${roomId}`;
 
 	if (forceH264)
 		url = `${url}&forceH264=true`;
